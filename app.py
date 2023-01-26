ca059fe6a10d0010862e4d7051e73ee97cf43f12
bhoff <bruce.hoff@sagebase.org>
2023-01-25 16:03:11 -0800

    comment about prod' deployment

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
+# Note, for production deployment, one approach is to use Talisman:
+# https://github.com/GoogleCloudPlatform/flask-talisman
+
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
